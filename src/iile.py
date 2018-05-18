bfb2152f40224dd4543cbeaa847c8f800369a76f
giuliojiang <giuliojiang@gmail.com>
2018-05-18 18:00:56 +0100

    Add camera fov

--- src/iile.py
+++ src/iile.py
@@ -7,6 +7,7 @@ import bpy
 from bl_ui import (
     properties_render,
     properties_material,
+    properties_data_camera
 )
 from bpy.types import Menu, Panel
 import os
@@ -14,7 +15,9 @@ import subprocess
 import math
 import time
 
-DEFAULT_IILE_PROJECT_PATH = "/home/gj/git/pbrt-v3-IISPT"
+DEFAULT_IILE_PROJECT_PATH = "/home/gj/git/pbrt-v3-IISPT/build"
+
+# Utilities ===============================================================================
 
 def runCmd(cmd, stdout=None, cwd=None):
     stdoutInfo = ""
@@ -32,6 +35,157 @@ def appendFile(sourcePath, destFile):
         destFile.write(line)
     sourceFile.close()
 
+# Scene parser ===================================================================================
+
+TABSIZE = 4
+
+def lineIndentTabs(l):
+    count = 0
+    for c in l:
+        if c == ' ':
+            count += 1
+        else:
+            break
+    if (count % TABSIZE) != 0:
+        return 0
+    return count / TABSIZE
+
+def indentBy(l, levels):
+    idt = ""
+    for i in range(TABSIZE * levels):
+        idt += " "
+    return idt + l
+
+# A block is a collection of lines part of a logical block,
+# for example in MakeNamedMaterial or in AttributeBegin
+class SceneBlock():
+
+    def __init__(self, lines):
+        self.lines = lines
+
+    # Returns the string representation for writing to file
+    # There is no trailing \n
+    def toString(self):
+        return "\n".join(self.lines)
+
+    # Returns None if this type of block is not recognized
+    def getBlockType(self):
+        if len(self.lines) == 0:
+            return None
+        first = self.lines[0]
+        splt = first.split(" ")
+        if len(splt) != 0:
+            return splt[0]
+        else:
+            return None
+
+    # Replaces a line content
+    # if the line is at the specified <level> of indentation
+    # and if it starts with <startMatch>
+    def replaceLine(self, level, startMatch, newStr):
+        for i in range(len(self.lines)):
+            l = self.lines[i]
+            if lineIndentTabs(l) == level:
+                rawLine = l[(TABSIZE * level):]
+                if rawLine.startswith(startMatch):
+                    transformedLine = indentBy(newStr, level)
+                    self.lines[i] = transformedLine
+
+    # Matching function
+    def contains(self, level, startMatch):
+        for i in range(len(self.lines)):
+            l = self.lines[i]
+            if lineIndentTabs(l) == level:
+                rawLine = l[(TABSIZE * level):]
+                if rawLine.startswith(startMatch):
+                    return True
+        return False
+
+    def findLine(self, level, startMatch):
+        for i in range(len(self.lines)):
+            l = self.lines[i]
+            if lineIndentTabs(l) == level:
+                rawLine = l[(TABSIZE * level):]
+                if rawLine.startswith(startMatch):
+                    return rawLine
+        return None
+
+    # Finds AttributeBegin->AreaLightSource blocks
+    def isAreaLightSource(self):
+        return (self.getBlockType() == "AttributeBegin") and (self.contains(1, "AreaLightSource"))
+
+    def isMakeNamedMaterial(self):
+        return self.getBlockType() == "MakeNamedMaterial"
+
+    def getMaterialDefinitionName(self):
+        first = self.lines[0].rstrip()
+        splt = first.split(" ")
+        return " ".join(splt[1:]).replace('"', '')
+
+    def clearBody(self):
+        if len(self.lines) >= 1:
+            self.lines = self.lines[0:1]
+
+    def getAssignedMaterial(self):
+        if self.getBlockType() == "AttributeBegin":
+            line = self.findLine(1, "NamedMaterial")
+            splt = line.split(" ")
+            if len(splt) > 0:
+                selection = splt[1:]
+                return " ".join(selection).replace('"', '')
+            else:
+                return None
+        else:
+            return None
+
+    def appendLine(self, level, content):
+        self.lines.append(indentBy(content, level))
+
+# Represents the entire parsed scenefile
+class SceneDocument():
+
+    def __init__(self):
+        self.blocks = []
+
+    def parse(self, filepath):
+        f = open(filepath, "r")
+
+        sceneBlocks = []
+        currentBlock = []
+
+        for line in f:
+            line = line[:-1]
+            if lineIndentTabs(line) > 0:
+                currentBlock.append(line)
+            else:
+                # Collect previous block
+                if len(currentBlock) > 0:
+                    sceneBlocks.append(SceneBlock(currentBlock))
+                # Start a new block
+                currentBlock = []
+                currentBlock.append(line)
+
+        # Collect last block
+        if len(currentBlock) > 0:
+            sceneBlocks.append(SceneBlock(currentBlock))
+
+        f.close()
+
+        self.blocks = sceneBlocks
+
+    def getBlocks(self):
+        return self.blocks
+
+    def write(self, outPath):
+        outFile = open(outPath, 'w')
+        for block in self.blocks:
+            outFile.write("{}\n".format(block.toString()))
+        outFile.close()
+
+# Scene parser end =============================================================
+
+# Render engine ================================================================================
+
 class IILERenderEngine(bpy.types.RenderEngine):
     bl_idname = "iile_renderer" # internal name
     bl_label = "IILE Renderer" # Visible name
@@ -46,8 +200,8 @@ class IILERenderEngine(bpy.types.RenderEngine):
         print("Starting render, resolution {} {}".format(sx, sy))
 
         # Compute pbrt executale path
-        pbrtExecPath = os.path.join(scene.iilePath, "build", "pbrt")
-        obj2pbrtExecPath = os.path.join(scene.iilePath, "build", "obj2pbrt")
+        pbrtExecPath = os.path.join(scene.iilePath, "pbrt")
+        obj2pbrtExecPath = os.path.join(scene.iilePath, "obj2pbrt")
 
         # Get the output path
         outDir = bpy.data.scenes["Scene"].render.filepath
@@ -98,13 +252,18 @@ class IILERenderEngine(bpy.types.RenderEngine):
         runCmd(cmd, stdout=outExp2PbrtFile, cwd=outDir)
         outExp2PbrtFile.close()
 
-        # Create headers and footers
+        # Create headers and footers -------------------------------------------------
         outSceneFile = open(outScenePath, "w")
 
         # Create headers
+        wline(outSceneFile, 'Film "image" "integer xresolution" {} "integer yresolution" {}'.format(sx, sy))
         wline(outSceneFile, 'Integrator "path"')
         wline(outSceneFile, 'Sampler "sobol" "integer pixelsamples" 1')
         wline(outSceneFile, 'Scale -1 1 1')
+
+        # Get camera
+        theCameraName = bpy.context.scene.camera.name
+        theCamera = bpy.data.cameras[theCameraName]
 
         # Write camera rotation
         cameraRotationAmount = bpy.context.scene.camera.rotation_axis_angle[0]
@@ -124,9 +283,8 @@ class IILERenderEngine(bpy.types.RenderEngine):
         wline(outSceneFile, 'Translate {} {} {}'.format(
             cameraLocX, cameraLocY, cameraLocZ))
 
-        # Write camera perspective
-        # TODO read fov from properties
-        wline(outSceneFile, 'Camera "perspective" "float fov" 70')
+        # Write camera fov
+        wline(outSceneFile, 'Camera "perspective" "float fov" [{}]'.format(math.degrees(theCamera.angle / 2.0)))
 
         # TODO write film size
 
@@ -140,6 +298,51 @@ class IILERenderEngine(bpy.types.RenderEngine):
         wline(outSceneFile, 'WorldEnd')
 
         outSceneFile.close()
+
+        # -----------------------------------------------------------
+        # Scene transformation
+        doc = SceneDocument()
+        doc.parse(outScenePath)
+        blocks = doc.getBlocks()
+
+        for block in blocks:
+
+            # Set area light emission color
+            if block.isAreaLightSource():
+                print("Processing an area light source")
+                matName = block.getAssignedMaterial()
+                print(matName)
+                matObj = bpy.data.materials[matName]
+                emitIntensity = matObj.emit
+                emitColor = [0.0, 0.0, 0.0]
+                emitColor[0] = emitIntensity * matObj.iileEmission[0]
+                emitColor[1] = emitIntensity * matObj.iileEmission[1]
+                emitColor[2] = emitIntensity * matObj.iileEmission[2]
+                block.replaceLine(3, '"rgb L"',
+                    '"rgb L" [ {} {} {} ]'.format(
+                        emitColor[0], emitColor[1], emitColor[2]))
+
+            # Set material properties
+            if block.isMakeNamedMaterial():
+                matName = block.getMaterialDefinitionName()
+                print("Processing material {}".format(matName))
+                matObj = bpy.data.materials[matName]
+                block.clearBody()
+                # Write material type
+                # MATTE
+                if matObj.iileMaterial == "MATTE":
+                    block.appendLine(2, '"string type" "matte"')
+                    # Diffuse color
+                    block.appendLine(2, '"rgb Kd" [ {} {} {} ]' \
+                        .format(matObj.iileMatteColor[0],
+                            matObj.iileMatteColor[1],
+                            matObj.iileMatteColor[2]))
+                else:
+                    raise Exception("Unrecognized material {}".format(
+                        matObj.iileMaterial))
+
+
+        doc.write(outScenePath)
 
         print("Rendering finished.")
         result = self.begin_result(0, 0, sx, sy)
@@ -176,8 +379,8 @@ class MATERIAL_PT_material(properties_material.MaterialButtonsPanel, Panel):
 
         layout.prop(mat, "iileMaterial", text="material")
 
-        if mat.iileMaterial == "DIFFUSE":
-            layout.prop(mat, "diffuse_color", text="")
+        if mat.iileMaterial == "MATTE":
+            layout.prop(mat, "iileMatteColor", text="")
 
 class MATERIAL_PT_emission(properties_material.MaterialButtonsPanel, Panel):
     bl_label = "Emission"
@@ -213,8 +416,19 @@ def register():
         name="IILE Material",
         description="Material type",
         items=[
-            ("DIFFUSE", "Diffuse", "Lambertian Diffuse Material")
+            ("MATTE", "Matte", "Lambertian Diffuse Material")
         ]
+    )
+
+    Mat.iileMatteColor = bpy.props.FloatVectorProperty(
+        name="Diffuse color",
+        description="Diffuse color",
+        subtype="COLOR",
+        precision=4,
+        step=0.01,
+        min=0.0,
+        soft_max=1.0,
+        default=(0.75, 0.75, 0.75)
     )
 
     Mat.iileEmission = bpy.props.FloatVectorProperty(
@@ -246,6 +460,9 @@ def register():
     bpy.utils.register_class(MATERIAL_PT_material)
     # Material emission
     bpy.utils.register_class(MATERIAL_PT_emission)
+
+    # Camera
+    properties_data_camera.DATA_PT_lens.COMPAT_ENGINES.add(IILERenderEngine.bl_idname)
 
 
 def unregister():
